13ad3c19938880175216a86b7c5fa44852253dd8
Jhao3190708 <glaraga_3190708@letran-calamba.edu.ph>
2023-02-27 18:35:59 +0800

    intial commint

---
+++ index.php
@@ -0,0 +1,36 @@
+<h1>Sample</h1>
+<?php
+$message = "Hellow Arz";
+echo $message;
+
+?>
+<hr>
+<?php
+
+$a = 1;
+$b = 3;
+echo $a + $b;
+?>
+
+<hr>
+
+<?php
+function add ($val1, $val2){
+    return $val1 + $val2;
+
+}
+   echo add(13,15);
+   
+   echo add (137,151);
+?>
+<hr>
+<?php
+function sub ($val1, $val2){
+    return $val1 - $val2;
+
+}
+   echo sub(13,15);
+   echo "  ";
+   echo sub (137,151);
+?>
+<hr>
